febf59288b6dc8b3c5154a83766ebbcc2fe1108b
SaurabhGodse <godsesaurabhgsm@gmail.com>
2020-02-22 21:17:16 +0530

    create role and demo completed

--- set_up/create_roles.py
+++ set_up/create_roles.py
@@ -16,11 +16,26 @@ from utils import group_IP
 
 
 QUERY = """
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM {user};
+REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM {user};
+REVOKE USAGE ON SCHEMA public FROM {user};
 DROP USER IF EXISTS {group};
 CREATE USER {user} WITH PASSWORD \'{pswd}\';
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO {group};
-GRANT USAGE ON ALL TABLES IN SCHEMA public TO {group};
 """
+QUERY1 = """
+GRANT CONNECT ON DATABASE {group} TO {user};
+GRANT ALL PRIVILEGES ON DATABASE {group} TO {user};
+GRANT USAGE ON SCHEMA public TO {user};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {user};
+"""
+
+# REVOKE CONNECT ON DATABASE {group} FROM {user};
+# REVOKE USAGE ON SCHEMA public FROM {user};
+
+# GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {user};
+# GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO {user};
+# GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO {group};
+# GRANT USAGE ON ALL TABLES IN SCHEMA public TO {group};
 # DROP USER IF EXISTS {group};
 # CREATE USER {user} WITH PASSWORD \'{pswd}\';
 # REVOKE USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public FROM {group};
@@ -61,10 +76,17 @@ if __name__== '__main__':
         conn = connect(group_IP(group))
         # print("Connected successfully...")
         query = QUERY.format(group=group, user = group, pswd=CREDENTIALS[group])
-        print(query)
+        # print(query)
         conn.autocommit = True
         conn.cursor().execute("DROP DATABASE IF EXISTS {};".format(group))
         conn.commit()
+        conn.cursor().execute(query)
+        conn.commit()
+        conn.cursor().execute("CREATE DATABASE {};".format(group))
+        conn.cursor().execute("REVOKE ALL PRIVILEGES ON DATABASE {} FROM public;".format(group))
+        conn.commit()
+        query = QUERY1.format(group=group, user = group, pswd=CREDENTIALS[group])
+
         conn.cursor().execute(query)
         conn.commit()
         conn.close()
